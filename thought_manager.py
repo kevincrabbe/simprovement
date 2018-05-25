26fe34b078e4a6fd001aeb00ee8247129eb468b3
Kevin <kevincrabbe@me.com>
2018-05-25 12:12:51 -0700

    stuff

---
+++ thought_manager.py
@@ -0,0 +1,9 @@
+class Thought:
+  def __init__(self, text, is_positive):
+    self.text = text
+    self.is_positive = is_positive
+
+class CostBenefitAnalysis:
+  def __init__(self, thought):
+    self.thought = thought
+
